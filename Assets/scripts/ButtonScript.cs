ffdbd31a4db7473d44f2938e7ebe83d484fab7e7
MykytaRachynskyi <33069638+MykytaRachynskyi@users.noreply.github.com>
2017-10-30 18:47:22 +0100

    Add files via upload

---
+++ Assets/scripts/ButtonScript.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+	public GameObject crossL, crossR, circle;
+	public int index;
+	public enum Value {none, circle, cross};
+	public Value cellValue;
+	public float speed = 10.0f;
+	public float animSpeed = 2f;
+	public AudioClip crossSFX, circleSFX;
+
+	public static int copyIndex = 0;
+	private static GameObject[] copy = new GameObject[15];
+
+	[SerializeField]private GameObject[] display = new GameObject[15];
+	private GameObject playerGMturn = null;
+	[SerializeField]private bool localTurn;
+	private static int activeCount = 0;
+	private Button btn = null;
+	private GMScript gmScript;
+	private Animation anim;
+	private AudioSource aSrc;
+
+	void Start ()
+	{
+		//Creating Button reference and listener for onClick
+		btn = this.GetComponent<Button> ();
+		btn.onClick.AddListener (Draw);
+
+		//Creating a reference to the Master Script
+		playerGMturn = GameObject.Find ("_GM");
+		gmScript = playerGMturn.GetComponent<GMScript>();
+
+		aSrc = GetComponent<AudioSource>();
+	}
+	void Update ()
+	{
+
+	}
+
+	void Draw ()
+	{
+		if (Input.GetMouseButtonUp (0)) {
+			localTurn = gmScript.playerOneTurn;
+			if (localTurn) 
+				 { DrawSign ("cross"); this.cellValue = Value.cross; } 
+			else { DrawSign ("circle"); this.cellValue = Value.circle; }
+			if (activeCount > 4)
+				gmScript.CheckVictory(localTurn);
+		}
+	}
+
+	void DrawSign (string sign)
+	{
+		Debug.Log(copyIndex);
+		if (sign == "circle") {
+			aSrc.PlayOneShot(circleSFX);
+			MakeCopy(circle);
+			UpdateTurnInfo();
+		} else if (sign == "cross") {
+			aSrc.PlayOneShot(crossSFX);
+			StartCoroutine(CrossAnim());
+			UpdateTurnInfo();
+		}
+	}
+
+	IEnumerator CrossAnim ()
+	{
+		CrossAnim("left");
+		yield return new WaitForSeconds(1f/animSpeed);
+		CrossAnim("right");
+	}
+
+	void CrossAnim (string side)
+	{
+		if (side == "left") MakeCopy(crossL);
+		else if (side == "right") MakeCopy(crossR);
+	}
+
+	void MakeCopy(GameObject sign)
+	{
+		copy[copyIndex] = Instantiate (sign, Vector3.zero, Quaternion.identity, this.transform) as GameObject;
+		copy[copyIndex].transform.localPosition = Vector2.zero;
+		anim = copy[copyIndex].GetComponent<Animation> ();
+		anim ["DrawLeft"].speed = animSpeed;
+		copyIndex++;
+	}
+
+	void UpdateTurnInfo ()
+	{
+		gmScript.TurnSwitch ();
+		btn.interactable = false;
+		activeCount++;
+	}
+
+	public static void Clear ()
+	{
+		for (int i = 0; i < copy.Length; i++) {
+			Destroy(copy[i]);
+			copy[i] = null;
+		} 
+		copyIndex = 0;
+
+	}
+}
---
+++ Assets/scripts/GMScript.cs
@@ -0,0 +1,180 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GMScript : MonoBehaviour {
+
+	public bool playerOneTurn;
+	public GameObject boardBlock;
+	public Transform canvasParent;
+	public int activeCount = 0;
+	public float blockSize = 250f;
+	public float boardOffset = 500f;
+	public CanvasGroup victoryScreen, boardScreen;
+	public Text victoryText;
+	public float waitTime;
+	public GameObject line;
+	public float animSpeed = 2f;
+
+	private GameObject copy;
+	private GameObject lineHolder;
+	private Text text;
+	private GameObject textGOobj;
+	private GameObject[] clone = new GameObject[9];
+	private ButtonScript[] instance = new ButtonScript[9];
+	private bool waited;
+	private Animation anim;
+
+	void Awake ()
+	{
+		CreateBoard();
+		playerOneTurn = true;
+	}
+
+	void Start ()
+	{
+		textGOobj = GameObject.FindWithTag("PlayerText");
+		text = textGOobj.GetComponent<Text>();
+		text.text = "Player 1";
+
+		//Hide the Vicory Screen
+		victoryScreen.alpha = 0f;
+		victoryScreen.interactable = false;
+		victoryScreen.blocksRaycasts = false;
+
+
+		anim = line.GetComponent<Animation> ();
+		anim ["DrawLeft"].speed = animSpeed;
+	}
+
+	void Update ()
+	{
+		
+	}
+
+	public void LoadMenu ()
+	{
+		SceneManager.LoadScene("menu");
+	}
+
+	void CreateBoard ()
+	{
+		Vector3 pos = Vector3.zero;
+		for (int i = 0; i < 9; i++) 
+		{
+			switch (i)
+			{
+				case 0: pos = new Vector3 (-blockSize, blockSize, 0) +  Vector3.right*boardOffset; break;
+				case 1: pos = new Vector3 (0,blockSize, 0)           +	Vector3.right*boardOffset; break;
+				case 2: pos = new Vector3 (blockSize,blockSize, 0)   + 	Vector3.right*boardOffset; break;
+				case 3: pos = new Vector3 (-blockSize, 0, 0)         +  Vector3.right*boardOffset; break;
+				case 4: pos = new Vector3 (0,0,0)					 +  Vector3.right*boardOffset; break;
+				case 5: pos = new Vector3 (blockSize,0,0)			 +	Vector3.right*boardOffset; break;
+				case 6: pos = new Vector3 (-blockSize,-blockSize,0)  + 	Vector3.right*boardOffset; break;
+				case 7: pos = new Vector3 (0,-blockSize,0)           +	Vector3.right*boardOffset; break;
+				case 8: pos = new Vector3 (blockSize,-blockSize,0)   + 	Vector3.right*boardOffset; break;
+			}
+			BoardBlockInfo(i,pos);
+		}
+	}
+
+	public void TurnSwitch ()
+	{
+		playerOneTurn = !playerOneTurn;
+		if (playerOneTurn) text.text = "Player 1"; else text.text = "Player 2";
+	}
+
+	public void CheckVictory(bool pOneVictory)
+	{
+		Debug.Log("Checking Victory...");
+			 if (instance[0].cellValue != ButtonScript.Value.none && 
+			     instance[0].cellValue == instance[1].cellValue && 
+				 instance[0].cellValue == instance[2].cellValue)
+				 StartCoroutine(GameOver(pOneVictory, "hTop"));
+		else if (instance[3].cellValue != ButtonScript.Value.none && 
+				 instance[3].cellValue == instance[4].cellValue && 
+				 instance[3].cellValue == instance[5].cellValue)
+			     StartCoroutine(GameOver(pOneVictory, "hMid"));
+		else if (instance[6].cellValue != ButtonScript.Value.none && 
+				 instance[6].cellValue == instance[7].cellValue && 
+				 instance[6].cellValue == instance[8].cellValue)
+				 StartCoroutine(GameOver(pOneVictory,"hBot"));
+		else if (instance[0].cellValue != ButtonScript.Value.none && 
+				 instance[0].cellValue == instance[4].cellValue && 
+				 instance[0].cellValue == instance[8].cellValue)
+				 StartCoroutine(GameOver(pOneVictory, "dLR"));
+		else if (instance[2].cellValue != ButtonScript.Value.none && 
+				 instance[2].cellValue == instance[4].cellValue && 
+				 instance[2].cellValue == instance[6].cellValue)
+				 StartCoroutine(GameOver(pOneVictory, "dRL"));
+		else if (instance[0].cellValue != ButtonScript.Value.none && 
+				 instance[0].cellValue == instance[3].cellValue && 
+				 instance[0].cellValue == instance[6].cellValue)
+				 StartCoroutine(GameOver(pOneVictory, "vLeft"));
+		else if (instance[1].cellValue != ButtonScript.Value.none && 
+				 instance[1].cellValue == instance[4].cellValue && 
+				 instance[1].cellValue == instance[7].cellValue)
+				 StartCoroutine(GameOver(pOneVictory, "vMid"));
+		else if (instance[2].cellValue != ButtonScript.Value.none && 
+				 instance[2].cellValue == instance[5].cellValue && 
+				 instance[2].cellValue == instance[8].cellValue)
+				 StartCoroutine(GameOver(pOneVictory, "vRight"));
+	}
+
+	void BoardBlockInfo (int i, Vector3 pos)
+	{
+		clone[i] = Instantiate (boardBlock, pos, Quaternion.identity, canvasParent) as GameObject;
+		instance[i] = clone[i].GetComponent<ButtonScript>();
+		instance[i].index = i;
+		clone[i].transform.localPosition = pos;
+	}
+
+	IEnumerator GameOver (bool pOneVictory, string victoryIndex)
+	{
+		boardScreen.interactable = false;
+		boardScreen.blocksRaycasts = false;
+		DrawLineAcross(victoryIndex);
+		yield return new WaitForSeconds (waitTime);
+		victoryScreen.alpha = 1f;
+		victoryScreen.interactable = true;
+		victoryScreen.blocksRaycasts = true;
+		if (pOneVictory) {
+			victoryText.text = ("Player 1 Wins!");
+		} else if (!pOneVictory) {
+			victoryText.text = ("Player 2 Wins!");
+		}
+	}
+
+	public void ResetGame ()
+	{
+		victoryScreen.alpha = 0f;
+		victoryScreen.interactable = false;
+		victoryScreen.blocksRaycasts = false;
+		boardScreen.interactable = true;
+		boardScreen.blocksRaycasts = true;
+		for (int i = 0; i < instance.Length; i++) {
+			instance [i].cellValue = ButtonScript.Value.none;
+			instance [i].GetComponent<Button> ().interactable = true;
+		}
+		playerOneTurn = true;
+		ButtonScript.Clear();
+		text.text = "Player 1";
+		Destroy(lineHolder);
+	}
+
+	void DrawLineAcross (string victoryIndex)
+	{
+		switch (victoryIndex) 
+		{
+			case "hTop":  lineHolder = Instantiate (line, instance[1].transform.position, Quaternion.identity, instance[1].transform) as GameObject; break;
+			case "hMid":  lineHolder = Instantiate (line, instance[4].transform.position, Quaternion.identity, instance[4].transform) as GameObject; break;
+			case "hBot":  lineHolder = Instantiate (line, instance[7].transform.position, Quaternion.identity, instance[7].transform) as GameObject; break;
+			case "dLR":   lineHolder = Instantiate (line, instance[4].transform.position, Quaternion.Euler(0,0,-45), instance[4].transform) as GameObject; break;
+			case "dRL":   lineHolder = Instantiate (line, instance[4].transform.position, Quaternion.Euler(0,0,180 + 45), instance[4].transform) as GameObject; break;
+			case "vLeft": lineHolder = Instantiate (line, instance[3].transform.position, Quaternion.Euler(0,0,-90), instance[1].transform) as GameObject; break;
+			case "vMid":  lineHolder = Instantiate (line, instance[4].transform.position, Quaternion.Euler(0,0,-90), instance[1].transform) as GameObject; break;
+			case "vRight":lineHolder = Instantiate (line, instance[5].transform.position, Quaternion.Euler(0,0,-90), instance[1].transform) as GameObject; break;
+		}
+	}
+}
